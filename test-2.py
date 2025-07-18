33703db84eebea1ac7e030cff47866474c589cf6
Pavan Parvatam <pavankumar41299@gmail.com>
2025-07-18 22:35:25 +0530

    missed test2 files updating again

---
+++ test-2.py
@@ -0,0 +1,18 @@
+# git status - used to check the newly added files or remove the files. (add, remove, update,..................)
+
+#git add test.py
+#git add test-2 .py 
+# 3 100????
+
+
+# git add -A (all)
+
+
+
+#  git commit -m "your message"
+
+
+# "1st time who are you "
+# git config --global user.email "pavankumar41299@gmail.com"
+
+# git config --global user.name "Pavan Parvatam"
